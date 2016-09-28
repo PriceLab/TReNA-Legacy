fb42128689fcf9fdaf16d6326e6bd9971401d93c
paul-shannon <paul.thurmond.shannon@gmail.com>
2016-09-28 14:41:02 -0700

    heatmaps added

--- inst/demos/trem2/go.R
+++ inst/demos/trem2/go.R
@@ -1,5 +1,6 @@
 library(RPostgreSQL)
 library(TReNA)
+library (RColorBrewer)
 #------------------------------------------------------------------------------------------------------------------------
 source("../../../../BDDS/trenadb/utils.R")
 #------------------------------------------------------------------------------------------------------------------------
@@ -24,10 +25,11 @@ if(!exists("tbl.genesmotifs"))
     tbl.genesmotifs <- dbGetQuery(db.trena, "select * from tfmotifs")
 
 #------------------------------------------------------------------------------------------------------------------------
-chrom <- trem2$chrom
-start <- trem2$start - 1000
-end   <- trem2$start + 1000
 target.gene <- "TREM2"
+shoulder <- 1000
+chrom <- trem2$chrom
+start <- trem2$start - shoulder
+end   <- trem2$start + shoulder
 
 tbl.h <- createHintTable(chrom, start, end)
 motifs <- unique(tbl.h$motif.h)
@@ -46,9 +48,37 @@ results <- lapply(matrices, function(mtx){
    mtx.sub <- mtx[genes.of.interest,]
    mtx.adjusted <- asinh(mtx.sub)
    trena <- TReNA(mtx.assay=mtx.adjusted, solver="lasso", quiet=FALSE)
-   solve(trena, target.gene, candidate.regulators, extraArgs =list(alpha=0.1, lambda=NULL))
+   tbl <- solve(trena, target.gene, candidate.regulators, extraArgs=list(alpha=0.1, lambda=NULL))
    })
 
+results2 <- lapply(names(results), function(name){
+    column.names <- colnames(results[[name]])
+    new.names <- paste(name, column.names, sep=".")
+    colnames(results[[name]]) <- new.names
+    results[[name]]
+    })
 
+names(results2) <- names(results)
 
-    
+all.tfs <- sort(unique(c(rownames(results2[[1]]),
+                         rownames(results2[[2]]),
+                         rownames(results2[[3]]))))
+all.conditions <- c(colnames(results2[[1]]),
+                    colnames(results2[[2]]),
+                    colnames(results2[[3]]))
+m <- matrix(0, nrow=length(all.tfs), ncol=length(all.conditions), dimnames=list(all.tfs, all.conditions))
+for(i in 1:length(results2)){
+    mtx <- as.matrix(results2[[i]])
+    row.names <- rownames(mtx)
+    column.names <- colnames(mtx)
+    print(row.names)
+    print(column.names)
+    print(mtx[row.names, column.names])
+    print(m[row.names, column.names])
+    m[row.names, column.names] <- mtx
+    }
+
+m.beta <- m[, grep("beta", colnames(m))]
+m.beta.noAll <- m.beta[, -(grep("all", colnames(m.beta)))]
+heatmap.2(m.beta, margins=c(10,10), trace='none')
+heatmap.2(m.beta.noAll, margins=c(10,10), trace='none', cexCol=2)
