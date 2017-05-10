18aafdc88870a34e8c9f5d51ec77eab8e8cc0e03
paul-shannon <paul.thurmond.shannon@gmail.com>
2017-05-10 16:49:18 -0700

    trena-viz roughed in

---
+++ R/TReNA-Viz.R
@@ -0,0 +1,105 @@
+#----------------------------------------------------------------------------------------------------
+cyjsBrowserFile <- system.file(package="TReNA", "scripts", "viz", "trenaViz.html")
+printf <- function(...) print(noquote(sprintf(...)))
+#----------------------------------------------------------------------------------------------------
+.TReNA.Viz <- setClass ("TReNA.Viz",
+                        representation = representation(model="data.frame"),
+                        contains = "BrowserVizClass",
+                        prototype = prototype (uri="http://localhost", 9000)
+                        )
+
+
+#----------------------------------------------------------------------------------------------------
+# constructor
+TReNA.Viz = function(portRange=11000:11025, host="localhost", title="TReNA-Viz", quiet=TRUE)
+{
+
+   model <- data.frame()
+   obj <- .TReNA.Viz(BrowserViz(portRange=portRange, host=host, title=title,
+                                quiet=quiet, browserFile=cyjsBrowserFile,
+                                httpQueryProcessingFunction=NULL),
+                    model=model)
+
+  while (!browserResponseReady(obj)){
+      Sys.sleep(.1)
+      }
+   if(!quiet) {
+      message(sprintf("BrowserViz ctor called from RCyjs ctor got browser response"))
+      print(getBrowserResponse(obj))
+      }
+
+   obj
+
+} # TReNA.Viz constructor
+#----------------------------------------------------------------------------------------------------
+# {elements: [
+#    {data: {id: 'a', score:5}, position: {x: 100, y: 200}},
+#    {data: {id: 'b', score:100}, position: {x: 200, y: 200}},
+#    {data: {id: 'e1', source: 'a', target: 'b'}}
+#    ],  // elements array
+# layout: { name: 'preset'},
+# style: [{selector: 'node', style: {'content': 'data(id)'}}]
+# }
+.graphToJSON <- function(g)
+{
+    x <- '{"elements": [';
+    nodes <- nodes(g)
+    edgeNames <- edgeNames(g)
+    edges <- strsplit(edgeNames, "~")  # a list of pairs
+    edgeNames <- sub("~", "->", edgeNames)
+    names(edges) <- edgeNames
+
+    noa.names <- names(nodeDataDefaults(g))
+    eda.names <- names(edgeDataDefaults(g))
+    nodeCount <- length(nodes)
+    edgeCount <- length(edgeNames)
+
+    for(n in 1:nodeCount){
+       node <- nodes[n]
+       x <- sprintf('%s {"data": {"id": "%s"', x, node);
+       nodeAttributeCount <- length(noa.names)
+       for(i in seq_len(nodeAttributeCount)){
+          noa.name <- noa.names[i];
+          value <-  nodeData(g, node, noa.name)[[1]]
+          if(is.numeric(value))
+             x <- sprintf('%s, "%s": %s', x, noa.name, value)
+          else
+             x <- sprintf('%s, "%s": "%s"', x, noa.name, value)
+          } # for i
+       x <- sprintf('%s}', x)     # close off this node data element
+       if(all(c("xPos", "yPos") %in% noa.names)){
+           xPos <- as.integer(nodeData(g, node, "xPos"))
+           yPos <- as.integer(nodeData(g, node, "yPos"))
+           x <- sprintf('%s, "position": {"x": %d, "y": %d}', x, xPos, yPos)
+           } # add position element
+       x <- sprintf('%s}', x)     # close off this node data element
+       if(n != nodeCount)
+           x <- sprintf("%s,", x)  # another node coming, add a comma
+       } # for n
+
+    for(e in seq_len(edgeCount)) {
+       edgeName <- edgeNames[e]
+       edge <- edges[[e]]
+       sourceNode <- edge[[1]]
+       targetNode <- edge[[2]]
+       x <- sprintf('%s, {"data": {"id": "%s", "source": "%s", "target": "%s"', x, edgeName, sourceNode, targetNode);
+       edgeAttributeCount <- length(eda.names)
+       for(i in seq_len(edgeAttributeCount)){
+          eda.name <- eda.names[i];
+          value <-  edgeData(g, sourceNode, targetNode, eda.name)[[1]]
+          if(is.numeric(value))
+             x <- sprintf('%s, "%s": %s', x, eda.name, value)
+          else
+             x <- sprintf('%s, "%s": "%s"', x, eda.name, value)
+          } # for each edgeAttribute
+       x <- sprintf('%s}}', x)     # close off this edge data element
+       } # for e
+
+    x <- sprintf("%s]}", x)
+
+    x
+
+} # .graphToJSON
+#------------------------------------------------------------------------------------------------------------------------
+
+
---
+++ inst/unitTests/test_TReNA-Viz.R
@@ -0,0 +1,80 @@
+library(TReNA)
+library(RUnit)
+#----------------------------------------------------------------------------------------------------
+# the vrk2 promoter snp
+# chr2:57907313-57907333
+sequence <- "ACCAGCATGCAAATTAGACAA"
+#----------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+   test_basicConstructor()
+
+} # runTests
+#------------------------------------------------------------------------------------------------------------------------
+test_basicConstructor <- function(reuse=FALSE)
+{
+
+   viz <- TReNA.Viz()
+
+} # test_basicConstructor
+#----------------------------------------------------------------------------------------------------
+test_addDemoGraph <- function()
+{
+   printf("--- test_addDemoGraph")
+
+} # test_addDemoGraph
+#----------------------------------------------------------------------------------------------------
+test_.graphToJSON <- function()
+{
+   printf("--- test.graphToJSON")
+
+   g <- graphNEL(edgemode='directed')
+
+   nodeDataDefaults(g, attr='label') <- 'default node label'
+   nodeDataDefaults(g, attr='type') <- 'default node label'
+   nodeDataDefaults(g, attr='count') <- 0
+   nodeDataDefaults(g, attr='score') <- 0.0
+
+   edgeDataDefaults(g, attr='edgeType') <- 'undefined'
+   edgeDataDefaults(g, attr='count') <- 0
+   edgeDataDefaults(g, attr='score') <- 0.0
+
+   g <- graph::addNode('A', g)
+   g <- graph::addNode('B', g)
+   g <- graph::addNode('C', g)
+
+   all.nodes <- nodes(g)
+   nodeData(g, c('A', 'B', 'C'), 'type') <- c('t_one', 't_two', 't_three')
+   nodeData(g, all.nodes, 'label') <- all.nodes
+   nodeData(g, all.nodes, 'score') <- runif(3, 0, 1)
+   nodeData(g, all.nodes, 'count') <- sample(1:10, 3)
+
+     # now add 1 edge
+   g <- graph::addEdge('A', 'B', g)
+   g <- graph::addEdge('B', 'C', g)
+   g <- graph::addEdge('C', 'A', g)
+
+   edgeData(g, 'A', 'B', 'edgeType') <- 'et_one'
+   edgeData(g, 'B', 'C', 'edgeType') <- 'et_two'
+
+   edgeData(g, 'A', 'B', 'score') <- runif(1, 0, 1)
+   edgeData(g, 'C', 'A', 'score') <- runif(1, 0, 1)
+
+   edgeData(g, 'B', 'C', 'count') <- sample(1:10, 1)
+   edgeData(g, 'C', 'A', 'count') <- sample(1:10, 1)
+
+   g.json <- TReNA:::.graphToJSON(g)
+      # a pretty good check, but only an assist to the real test, which is to load this the
+      # browser with cy.json(JSON.parse(<g.json string>))
+
+   tbl <- fromJSON(g.json)[[1]][[1]]
+   checkEquals(nrow(tbl), length(nodes(g)) + length(edgeNames(g)))
+   checkEquals(colnames(tbl), c("id", "label", "type", "count", "score", "source", "target", "edgeType"))
+   checkEquals(unlist(lapply(tbl, class), use.names=FALSE),
+               c("character", "character", "character", "integer", "numeric", "character", "character", "character"))
+   checkEquals(tbl$type[1:3], c("t_one", "t_two", "t_three"))
+   checkEquals(tbl$edgeType[4:6], c("et_one", "et_two", "undefined"))
+
+
+} # test_.graphToJSON
+#----------------------------------------------------------------------------------------------------
